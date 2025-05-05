a2f1c22b3e3fb2ae7c39d5640b76bc2748226ef1
mabubakr1113 <mohammadabubakr1113@gmail.com>
2025-05-05 11:28:49 +0500

    Setup Express server and basic app configuration

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("App is running is running");
+});
+
+export default app;
